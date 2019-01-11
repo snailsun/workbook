1dc6385695ea743411e4a9605252a0db1c1a801f
Administrator <Administrator@PC-20180331IVYI>
2019-01-11 18:01:00 +0800

    actuator使用2.0之前

---
+++ actuatorDome/src/main/java/com/example/actuatorDome/ActuatorDomeApplication.java
@@ -0,0 +1,14 @@
+package com.example.actuatorDome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorDomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorDomeApplication.class, args);
+	}
+
+}
+
